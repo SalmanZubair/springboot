7b53aff001b5ab4eb7cccc436b59b31d866e3045
salma <salma@192.168.0.12>
2020-05-29 16:13:54 -0500

    Updates

--- course-api/src/main/java/io/javabrains/courseapi/service/TopicService.java
+++ course-api/src/main/java/io/javabrains/courseapi/service/TopicService.java
@@ -4,19 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.javabrains.courseapi.dao.TopicRepository;
 import io.javabrains.courseapi.model.Topic;
 
 @Service
 public class TopicService {
 
+	@Autowired
+	private TopicRepository topicRepo;
+	
 	List<Topic> topics = new ArrayList<>(
 			Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
 					new Topic("java", "Core Java", "Core Java Description"),
 					new Topic("javaScript", "JavaScript", "JavaScript Description")));
 
 	public List<Topic> getAllTopics() {
+
+		List<Topic> topics = new ArrayList<>();
+		topicRepo.findAll().forEach(topics::add);
 		return topics;
 	}
 
@@ -25,7 +33,7 @@ public class TopicService {
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepo.save(topic);
 	}
 
 	public void updateTopic(String id, Topic topic) {
--- course-api/src/test/java/io/javabrains/courseapi/CourseApiApplicationTests.java
+++ course-api/src/test/java/io/javabrains/courseapi/CourseApiApplicationTests.java
@@ -1,4 +1,4 @@
-package io.javabrains.courseapi;
+/*package io.javabrains.courseapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class CourseApiApplicationTests {
 	}
 
 }
+*/
